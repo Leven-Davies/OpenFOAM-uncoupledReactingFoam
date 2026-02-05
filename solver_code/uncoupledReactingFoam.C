e2ff015961be504b3ec8f8af2ee89ccc5f1c4732
Leven Davies <ld642@login-q-2.data.cluster>
2026-02-05 19:53:21 +0000

    Added uncoupledReactingFoam source code to repository

---
+++ solver_code/uncoupledReactingFoam.C
@@ -0,0 +1,159 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2017 OpenFOAM Foundation
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    reactingFoam
+
+Group
+    grpCombustionSolvers
+
+Description
+    Solver for combustion with chemical reactions.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "turbulentFluidThermoModel.H"
+#include "psiReactionThermo.H"
+#include "CombustionModel.H"
+#include "multivariateScheme.H"
+#include "pimpleControl.H"
+#include "pressureControl.H"
+#include "fvOptions.H"
+#include "localEulerDdtScheme.H"
+#include "fvcSmooth.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::addNote
+    (
+        "Solver for combustion with chemical reactions"
+    );
+
+    #include "postProcess.H"
+
+    #include "addCheckCaseOptions.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createControl.H"
+    #include "createTimeControls.H"
+    #include "initContinuityErrs.H"
+    #include "createFields.H"
+    #include "createFieldRefs.H"
+
+    turbulence->validate();
+
+    if (!LTS)
+    {
+        #include "compressibleCourantNo.H"
+        #include "setInitialDeltaT.H"
+    }
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+        #include "readTimeControls.H"
+
+        if (LTS)
+        {
+            #include "setRDeltaT.H"
+        }
+        else
+        {
+            #include "compressibleCourantNo.H"
+            #include "setDeltaT.H"
+        }
+
+        ++runTime;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        // --- UPDATED: rhoEqn is solved using the 'frozen' density from previous step
+        #include "rhoEqn.H"
+
+        tmp<fv::convectionScheme<scalar>> mvConvection
+        (
+            fv::convectionScheme<scalar>::New
+            (
+                mesh,
+                fields,
+                phi,
+                mesh.divScheme("div(phi,Yi_h)")
+            )
+        );
+
+        while (pimple.loop())
+        {   
+            // --- 1. SOLVE MOMENTUM AND PRESSURE (THE FLOW)
+            // This represents the independent jet evolution described by Garmory
+            #include "UEqn.H"
+            #include "EEqn.H"
+
+            // --- Pressure corrector loop
+            while (pimple.correct())
+            {
+                if (pimple.consistent())
+                {
+                    #include "pcEqn.H"
+                }
+                else
+                {
+                    #include "pEqn.H"
+                }
+            }
+
+            if (pimple.turbCorr())
+            {
+                turbulence->correct();
+            }
+            
+            // --- 2. SOLVE TRANSPORT AND CHEMISTRY (THE EVOLUTION)
+            // Moved outside the pressure corrector to decouple feedback.
+            // EEqn.H has Qdot commented out to decouple chemical thermal feedback.
+            #include "YEqn.H"
+
+
+            // optional addition -- this makes the solver a compressible solver
+            // rho = thermo.rho();
+        }
+
+        runTime.write();
+
+        runTime.printExecutionTime(Info);
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
